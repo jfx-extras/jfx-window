b676e6c4ea6e11a5fba88457890b980cc184175a
Pavel Erokhin <MairwunNx@gmail.com>
2019-03-20 19:06:01 +0300

    Updated Gradle version.
    Added java, java-library, maven-publish, dokka, shadow
    plugins.

--- jfx-window/build.gradle.kts
+++ jfx-window/build.gradle.kts
@@ -2,6 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
+    java
+    `java-library`
+    `maven-publish`
+    id("org.jetbrains.dokka") version "0.9.17"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "org.jfxextras"
