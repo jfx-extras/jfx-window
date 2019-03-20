5e83d0244210232aaa753d5a59eb8cd9994bceef
Pavel Erokhin <MairwunNx@gmail.com>
2019-03-20 18:38:51 +0300

    Added WindowOptions class with default options.

---
+++ jfx-window/src/main/kotlin/jfxwindow/base/WindowOptions.kt
@@ -0,0 +1,106 @@
+package jfxwindow.base
+
+import javafx.geometry.NodeOrientation
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import jfxwindow.enums.ButtonStyle
+import jfxwindow.enums.ShadowStyle
+import jfxwindow.enums.TitleAlignment
+import jfxwindow.enums.TitleShadowDepth
+import java.io.File
+
+internal class WindowOptions(stageInstance: Stage) {
+    private val domain: String = File(
+        WindowOptions::class.java.protectionDomain
+            .codeSource
+            .location.toURI()
+            .path
+    ).name
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var animationDuration: Double = 0.2
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var smoothColorAnimation: Boolean = true
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var borderIsVisible: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var borderColor: Color = Color.web("#464646")
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var borderInactiveColor: Color = Color.web("#A5A5A5")
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var contextMenuIsEnabled: Boolean = true
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal lateinit var icon: String
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var iconIsVisible: Boolean = false
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var isClosable: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var closeButtonIsVisible: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var isMaximizable: Boolean = stageInstance.isResizable
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var maximizeButtonIsVisible: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var isMinimizable: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var minimizeButtonIsVisible: Boolean = true
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var isDraggable: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var isResizable: Boolean = stageInstance.isResizable
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var resizeLimit: Boolean = true
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var saveWindowPosition: Boolean = false
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var stage: Stage = stageInstance
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var svgIconZoom: Double = 0.3
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleAlignment: TitleAlignment = TitleAlignment.LEFT
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleIsVisible: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleText: String = if (stageInstance.title != null) stageInstance.title else domain
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleTextSize: Double = 12.0
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleTextFont: String = "Segoe UI"
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarBackground: Color = Color.web("#FFFFFF")
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarInactiveBackground: Color = Color.web("#FFFFFF")
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarOrder: NodeOrientation = NodeOrientation.LEFT_TO_RIGHT
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarShadowDepth: TitleShadowDepth = TitleShadowDepth.DEPTH0
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarInactiveShadowDepth: TitleShadowDepth = TitleShadowDepth.DEPTH0
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var titleBarBottomBorderIsVisible: Boolean = false
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var toolButtonsStyle: ButtonStyle = ButtonStyle.WIN32
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var toolButtonsTooltipIsEnabled: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var toolButtonsHoverColor: Color = Color.color(0.0, 0.0, 0.0, 0.1)
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var toolButtonsPressedColor: Color = Color.color(0.0, 0.0, 0.0, 0.2)
+
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var windowShadowIsEnabled: Boolean = true
+    @set:JvmSynthetic @get:JvmSynthetic
+    internal var windowShadowType: ShadowStyle = ShadowStyle.WIN32
+}
