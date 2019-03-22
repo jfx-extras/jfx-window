4ad280a46a3346c211216cebe9b973555503f14c
Pavel Erokhin <MairwunNx@gmail.com>
2019-03-22 10:30:07 +0300

    Added demo for test children returning.

--- jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/EntryPoint.java
+++ jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/EntryPoint.java
@@ -1,16 +1,22 @@
 package com.mairwunnx.jdemo;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import jfxwindow.base.Window;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class EntryPoint extends Application {
     @Override
-    public void start(Stage stage) {
-        Scene scene = new Scene(new Pane(), 640, 480);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getClassLoader().getResource("demo.fxml")));
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         Window window = new Window(stage)
