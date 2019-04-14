20eea21a1b0cfe8fc5485f6228d379c51a2a3400
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-14 23:51:48 +0300

    Added Icon section example.

--- jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
+++ jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
@@ -1,5 +1,6 @@
 package com.mairwunnx.jdemo;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
@@ -14,6 +15,11 @@ public class Controller {
     public TextField titleAlignTextField;
     public TextArea titleFontResult;
     public TextField titleFontName;
+    public TextField iconPathResult;
+    public TextField iconPath;
+    public TextField iconIsSvgResult;
+    public TextField svgIconZoomResult;
+    public TextField svgIconZoom;
 
     public void getTitleText(MouseEvent mouseEvent) {
         titleTextResult.setText(EntryPoint.window.getWindowBase().getTitlePart().getTitleText());
@@ -47,5 +53,33 @@ public class Controller {
     public void setTitleFont(MouseEvent mouseEvent) {
         String[] font = ";".split(titleFontName.getText());
         EntryPoint.window.getWindowBase().getTitlePart().setTitleTextFont(new Font(font[0], Double.parseDouble(font[1])));
+    }
+
+    public void getIconPath(MouseEvent mouseEvent) {
+        iconPathResult.setText(EntryPoint.window.getWindowBase().getIconPart().getIcon());
+    }
+
+    public void setIconPath(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getIconPart().setIcon(iconPath.getText());
+    }
+
+    public void changeIconVisible(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getIconPart().setIconIsVisible(!EntryPoint.window.getWindowBase().getIconPart().getIconIsVisible());
+    }
+
+    public void getIconIsSvg(MouseEvent mouseEvent) {
+        iconIsSvgResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getIconPart().getIconIsSvg()));
+    }
+
+    public void getSvgIconZoom(MouseEvent mouseEvent) {
+        svgIconZoomResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getIconPart().getSvgIconZoom()));
+    }
+
+    public void setSvgIconZoom(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getIconPart().setSvgIconZoom(Double.parseDouble(svgIconZoom.getText()));
+    }
+
+    public void removeIcon(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getIconPart().removeIcon();
     }
 }
--- jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/EntryPoint.java
+++ jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/EntryPoint.java
@@ -34,7 +34,6 @@ public class EntryPoint extends Application {
                 .titleTextFont(Font.loadFont(String.valueOf(getClass().getClassLoader().getResource("segoeui.ttf")), 12.0))
                 .titleText("jfx-window")
                 .icon("jfx-logo.png")
-                .iconIsVisible(false)
                 .saveWindowPosition(true)
                 .borderColor(Color.BLACK)
                 .borderInactiveColor(Color.DARKGRAY)
@@ -58,7 +57,7 @@ public class EntryPoint extends Application {
         window.getWindowBase().getTitlePart().setTitleText("Size: " + width + "x" + height);
         window.getWindowBase().getWindowPart().calculateMinWidthSizeByTitleBar();
         // ↑ It can be applied if you want to set min width size (label width + button widths)
-        // just comment 62 line for see what be if disable it, and you can also change "Size:" to another text.
+        // just comment 58 line for see what be if disable it, and you can also change "Size:" to another text.
         // but you can just set fixed minWidth on stage.
     }
 
