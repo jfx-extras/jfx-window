4edeb78f7aeb4f65c25235c05a411283acda330e
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-15 18:20:43 +0300

    Added example for window section.
    Finded bugs:
     - isResizable can be enabled in maximized mode.
     - isResizable enable again after changing isMaximized value.
     - isResizable not cause disabling Maxmimize button.
     - Not binding value for disabled opacity buttons.

--- jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
+++ jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 import jfxwindow.enums.TitleAlignment;
 
 public class Controller {
@@ -20,6 +21,10 @@ public class Controller {
     public TextField iconIsSvgResult;
     public TextField svgIconZoomResult;
     public TextField svgIconZoom;
+    public TextField animationDurationResult;
+    public TextField animationDuration;
+    public TextField buttonDisableOpacityResult;
+    public TextField buttonDisableOpacity;
 
     public void getTitleText(MouseEvent mouseEvent) {
         titleTextResult.setText(EntryPoint.window.getWindowBase().getTitlePart().getTitleText());
@@ -81,5 +86,49 @@ public class Controller {
 
     public void removeIcon(MouseEvent mouseEvent) {
         EntryPoint.window.getWindowBase().getIconPart().removeIcon();
+    }
+
+    public void getAnimationDuration(MouseEvent mouseEvent) {
+        animationDurationResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getWindowPart().getAnimationDuration().toMillis()));
+    }
+
+    public void setAnimationDuration(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setAnimationDuration(Duration.millis(Double.parseDouble(animationDuration.getText())));
+    }
+
+    public void changeAnimationDuration(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setSmoothColorAnim(!EntryPoint.window.getWindowBase().getWindowPart().getSmoothColorAnim());
+    }
+
+    public void changeSavingPosition(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setSaveWindowPosition(!EntryPoint.window.getWindowBase().getWindowPart().getSaveWindowPosition());
+    }
+
+    public void changeIsResizable(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setResizable(!EntryPoint.window.getWindowBase().getWindowPart().isResizable());
+    }
+
+    public void changeIsDraggable(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setDraggable(!EntryPoint.window.getWindowBase().getWindowPart().isDraggable());
+    }
+
+    public void changeIsMaximizable(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setMaximizable(!EntryPoint.window.getWindowBase().getWindowPart().isMaximizable());
+    }
+
+    public void changeIsMinimizable(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setMinimizable(!EntryPoint.window.getWindowBase().getWindowPart().isMinimizable());
+    }
+
+    public void changeIsClosable(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setClosable(!EntryPoint.window.getWindowBase().getWindowPart().isClosable());
+    }
+
+    public void getButtonDisableOpacity(MouseEvent mouseEvent) {
+        buttonDisableOpacityResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getWindowPart().getDisabledOpacity()));
+    }
+
+    public void setButtonDusableOpacityResult(MouseEvent mouseEvent) {
+        EntryPoint.window.getWindowBase().getWindowPart().setDisabledOpacity(Double.parseDouble(buttonDisableOpacity.getText()));
     }
 }
