b7bdffd62133167bc89ba583be799672444340af
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-15 18:55:49 +0300

    Added example for TitleBar section.
    Finded bugs:
     - Context menu not can be disabled.

--- jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
+++ jfx-window.jdemo/src/main/java/com/mairwunnx/jdemo/Controller.java
@@ -1,13 +1,18 @@
 package com.mairwunnx.jdemo;
 
+import javafx.geometry.NodeOrientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 import jfxwindow.enums.TitleAlignment;
+import jfxwindow.enums.TitleShadowDepth;
+
+import static com.mairwunnx.jdemo.EntryPoint.*;
 
 public class Controller {
     public TextField titleTextResult;
@@ -25,110 +30,142 @@ public class Controller {
     public TextField animationDuration;
     public TextField buttonDisableOpacityResult;
     public TextField buttonDisableOpacity;
+    public TextField titleBackgroundResult;
+    public TextField titleBackground;
+    public TextField shadowDepthResult;
+    public TextField shadowDepth;
 
     public void getTitleText(MouseEvent mouseEvent) {
-        titleTextResult.setText(EntryPoint.window.getWindowBase().getTitlePart().getTitleText());
+        titleTextResult.setText(window.getWindowBase().getTitlePart().getTitleText());
     }
 
     public void setTitleText(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getTitlePart().setTitleText(titleTextTextField.getText());
+        window.getWindowBase().getTitlePart().setTitleText(titleTextTextField.getText());
     }
 
     public void changeTitleVisibility(MouseEvent mouseEvent) {
         ToggleButton toggleButton = (ToggleButton) mouseEvent.getSource();
-        EntryPoint.window.getWindowBase().getTitlePart().setTitleIsVisible(toggleButton.isSelected());
+        window.getWindowBase().getTitlePart().setTitleIsVisible(toggleButton.isSelected());
     }
 
     public void changeTitleAlign(MouseEvent mouseEvent) {
         if (titleAlignTextField.getText().toLowerCase().equals(TitleAlignment.CENTER.name().toLowerCase())) {
-            EntryPoint.window.getWindowBase().getTitlePart().setTitleAlignment(TitleAlignment.CENTER);
+            window.getWindowBase().getTitlePart().setTitleAlignment(TitleAlignment.CENTER);
         } else {
-            EntryPoint.window.getWindowBase().getTitlePart().setTitleAlignment(TitleAlignment.LEFT);
+            window.getWindowBase().getTitlePart().setTitleAlignment(TitleAlignment.LEFT);
         }
     }
 
     public void getTitleAlign(MouseEvent mouseEvent) {
-        titleAlignResult.setText(EntryPoint.window.getWindowBase().getTitlePart().getTitleAlignment().name());
+        titleAlignResult.setText(window.getWindowBase().getTitlePart().getTitleAlignment().name());
     }
 
     public void getTitleFont(MouseEvent mouseEvent) {
-        titleFontResult.setText(EntryPoint.window.getWindowBase().getTitlePart().getTitleTextFont().toString());
+        titleFontResult.setText(window.getWindowBase().getTitlePart().getTitleTextFont().toString());
     }
 
     public void setTitleFont(MouseEvent mouseEvent) {
         String[] font = ";".split(titleFontName.getText());
-        EntryPoint.window.getWindowBase().getTitlePart().setTitleTextFont(new Font(font[0], Double.parseDouble(font[1])));
+        window.getWindowBase().getTitlePart().setTitleTextFont(new Font(font[0], Double.parseDouble(font[1])));
     }
 
     public void getIconPath(MouseEvent mouseEvent) {
-        iconPathResult.setText(EntryPoint.window.getWindowBase().getIconPart().getIcon());
+        iconPathResult.setText(window.getWindowBase().getIconPart().getIcon());
     }
 
     public void setIconPath(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getIconPart().setIcon(iconPath.getText());
+        window.getWindowBase().getIconPart().setIcon(iconPath.getText());
     }
 
     public void changeIconVisible(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getIconPart().setIconIsVisible(!EntryPoint.window.getWindowBase().getIconPart().getIconIsVisible());
+        window.getWindowBase().getIconPart().setIconIsVisible(!window.getWindowBase().getIconPart().getIconIsVisible());
     }
 
     public void getIconIsSvg(MouseEvent mouseEvent) {
-        iconIsSvgResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getIconPart().getIconIsSvg()));
+        iconIsSvgResult.setText(String.valueOf(window.getWindowBase().getIconPart().getIconIsSvg()));
     }
 
     public void getSvgIconZoom(MouseEvent mouseEvent) {
-        svgIconZoomResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getIconPart().getSvgIconZoom()));
+        svgIconZoomResult.setText(String.valueOf(window.getWindowBase().getIconPart().getSvgIconZoom()));
     }
 
     public void setSvgIconZoom(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getIconPart().setSvgIconZoom(Double.parseDouble(svgIconZoom.getText()));
+        window.getWindowBase().getIconPart().setSvgIconZoom(Double.parseDouble(svgIconZoom.getText()));
     }
 
     public void removeIcon(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getIconPart().removeIcon();
+        window.getWindowBase().getIconPart().removeIcon();
     }
 
     public void getAnimationDuration(MouseEvent mouseEvent) {
-        animationDurationResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getWindowPart().getAnimationDuration().toMillis()));
+        animationDurationResult.setText(String.valueOf(window.getWindowBase().getWindowPart().getAnimationDuration().toMillis()));
     }
 
     public void setAnimationDuration(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setAnimationDuration(Duration.millis(Double.parseDouble(animationDuration.getText())));
+        window.getWindowBase().getWindowPart().setAnimationDuration(Duration.millis(Double.parseDouble(animationDuration.getText())));
     }
 
     public void changeAnimationDuration(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setSmoothColorAnim(!EntryPoint.window.getWindowBase().getWindowPart().getSmoothColorAnim());
+        window.getWindowBase().getWindowPart().setSmoothColorAnim(!window.getWindowBase().getWindowPart().getSmoothColorAnim());
     }
 
     public void changeSavingPosition(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setSaveWindowPosition(!EntryPoint.window.getWindowBase().getWindowPart().getSaveWindowPosition());
+        window.getWindowBase().getWindowPart().setSaveWindowPosition(!window.getWindowBase().getWindowPart().getSaveWindowPosition());
     }
 
     public void changeIsResizable(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setResizable(!EntryPoint.window.getWindowBase().getWindowPart().isResizable());
+        window.getWindowBase().getWindowPart().setResizable(!window.getWindowBase().getWindowPart().isResizable());
     }
 
     public void changeIsDraggable(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setDraggable(!EntryPoint.window.getWindowBase().getWindowPart().isDraggable());
+        window.getWindowBase().getWindowPart().setDraggable(!window.getWindowBase().getWindowPart().isDraggable());
     }
 
     public void changeIsMaximizable(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setMaximizable(!EntryPoint.window.getWindowBase().getWindowPart().isMaximizable());
+        window.getWindowBase().getWindowPart().setMaximizable(!window.getWindowBase().getWindowPart().isMaximizable());
     }
 
     public void changeIsMinimizable(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setMinimizable(!EntryPoint.window.getWindowBase().getWindowPart().isMinimizable());
+        window.getWindowBase().getWindowPart().setMinimizable(!window.getWindowBase().getWindowPart().isMinimizable());
     }
 
     public void changeIsClosable(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setClosable(!EntryPoint.window.getWindowBase().getWindowPart().isClosable());
+        window.getWindowBase().getWindowPart().setClosable(!window.getWindowBase().getWindowPart().isClosable());
     }
 
     public void getButtonDisableOpacity(MouseEvent mouseEvent) {
-        buttonDisableOpacityResult.setText(String.valueOf(EntryPoint.window.getWindowBase().getWindowPart().getDisabledOpacity()));
+        buttonDisableOpacityResult.setText(String.valueOf(window.getWindowBase().getWindowPart().getDisabledOpacity()));
     }
 
     public void setButtonDusableOpacityResult(MouseEvent mouseEvent) {
-        EntryPoint.window.getWindowBase().getWindowPart().setDisabledOpacity(Double.parseDouble(buttonDisableOpacity.getText()));
+        window.getWindowBase().getWindowPart().setDisabledOpacity(Double.parseDouble(buttonDisableOpacity.getText()));
+    }
+
+    public void getTitleBackground(MouseEvent mouseEvent) {
+        titleBackgroundResult.setText(String.valueOf(window.getWindowBase().getTitleBarPart().getTitleBackground()));
+    }
+
+    public void setTitleBackground(MouseEvent mouseEvent) {
+        window.getWindowBase().getTitleBarPart().setTitleBackground(Color.web(titleBackground.getText()));
+    }
+
+    public void changeTitleOrder(MouseEvent mouseEvent) {
+        if (window.getWindowBase().getTitleBarPart().getOrder() == NodeOrientation.LEFT_TO_RIGHT) {
+            window.getWindowBase().getTitleBarPart().setOrder(NodeOrientation.RIGHT_TO_LEFT);
+        } else {
+            window.getWindowBase().getTitleBarPart().setOrder(NodeOrientation.LEFT_TO_RIGHT);
+        }
+    }
+
+    public void changeContextMenuIsEnabled(MouseEvent mouseEvent) {
+        window.getWindowBase().getContextPart().setContextMenuIsEnabled(!window.getWindowBase().getContextPart().getContextMenuIsEnabled());
+    }
+
+    public void getShadowDepth(MouseEvent mouseEvent) {
+        shadowDepthResult.setText(window.getWindowBase().getTitleBarPart().getShadowDepth().name());
+    }
+
+    public void setShadowDepth(MouseEvent mouseEvent) {
+        window.getWindowBase().getTitleBarPart().setShadowDepth(TitleShadowDepth.valueOf("DEPTH" + shadowDepth.getText()));
     }
 }
