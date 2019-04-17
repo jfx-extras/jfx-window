998781121ff7c6e5dc9c908cfeccf12712de8c29
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-17 17:23:29 +0300

    Edited build.gradle for kdemo.
    Added EntryPoint kotlin file.
    Removed jfx-logo from jdemo.

--- jfx-window.kdemo/build.gradle.kts
+++ jfx-window.kdemo/build.gradle.kts
@@ -2,17 +2,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
+    application
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "org.jfxextras"
-version = "0.0.0.0"
+version = "8.0.0.0"
 
 repositories {
+    maven("https://kotlin.bintray.com/kotlinx")
+    mavenLocal()
     mavenCentral()
+    jcenter()
+}
+
+application {
+    group = "org.jfxextras"
+    version = "8.0.0.0"
+    applicationName = "jfx-window.kdemo"
+    mainClassName = "com.mairwunnx.kdemo.EntryPoint"
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jfxextras:jfxwindow:8.+")
 }
 
 tasks.withType<KotlinCompile> {
