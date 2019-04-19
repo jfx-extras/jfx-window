da0199c3364d7fb921c79727335339b320f8ccfb
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-19 22:48:31 +0300

    Updated kdemo for (changing title when do resize).

--- jfx-window.kdemo/src/main/kotlin/com/mairwunnx/kdemo/Application.kt
+++ jfx-window.kdemo/src/main/kotlin/com/mairwunnx/kdemo/Application.kt
@@ -6,6 +6,7 @@ import javafx.scene.text.Font
 import javafx.stage.Stage
 import javafx.stage.StageStyle
 import jfxwindow.base.Window
+import jfxwindow.enums.TitleAlignment
 import tornadofx.App
 
 class Application : App(BaseView::class) {
@@ -25,12 +26,41 @@ class Application : App(BaseView::class) {
         super.start(stage)
         window.create()
 
-        stage.minWidth = 0.0
+        stage.widthProperty().addListener { newValue ->
+            width = java.lang.Double.parseDouble(
+                newValue.toString().split("value: ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1].replace(
+                    "]",
+                    ""
+                )
+            )
+            calculateSize()
+        }
+        stage.heightProperty().addListener { newValue ->
+            height = java.lang.Double.parseDouble(
+                newValue.toString().split("value: ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1].replace(
+                    "]",
+                    ""
+                )
+            )
+            calculateSize()
+        }
+
+        //stage.minWidth = 0.0
         // ↑ it test of exhibiting incompatible size for stage.
         // if size so small, it make stage with default min size.
     }
 
+    private fun calculateSize() {
+        windowInstance.windowBase.titlePart.titleText = "Size: $width x $height"
+        windowInstance.windowBase.windowPart.calculateMinWidthSizeByTitleBar()
+        // ↑ It can be applied if you want to set min width size (label width + button widths)
+        // just comment 54 line for see what be if disable it, and you can also change "Size:" to another text.
+        // but you can just set fixed minWidth on stage.
+    }
+
     companion object {
         lateinit var windowInstance: Window
+        var width: Double = 0.0
+        var height: Double = 0.0
     }
 }
