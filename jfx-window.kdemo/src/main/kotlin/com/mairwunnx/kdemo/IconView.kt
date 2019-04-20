cae5563c7b632f292c65161ff1a37b3e15787293
Pavel Erokhin <MairwunNx@gmail.com>
2019-04-20 19:21:40 +0300

    Added demo for Icon section.
    Also Icon and Title and next views have split logics.

---
+++ jfx-window.kdemo/src/main/kotlin/com/mairwunnx/kdemo/IconView.kt
@@ -0,0 +1,155 @@
+package com.mairwunnx.kdemo
+
+import javafx.geometry.Insets
+import javafx.scene.control.TextField
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+import tornadofx.*
+import com.mairwunnx.kdemo.Application.Companion.windowInstance as window
+
+class IconView : View() {
+    private lateinit var iconSourceResult: TextField
+    private lateinit var iconSource: TextField
+    private lateinit var iconIsSvgResult: TextField
+    private lateinit var svgIconZoomResult: TextField
+    private lateinit var svgIconZoom: TextField
+
+    override val root: VBox = vbox {
+        minHeight = 10.0
+        prefWidth = 161.0
+        hgrow = Priority.ALWAYS
+
+        style {
+            backgroundColor = MultiValue(arrayOf(c("#fff")))
+        }
+
+        label("Icon") {
+            font = Font.font("Segoe UI Semilight", 29.0)
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 20.0))
+        }
+
+        button("get icon source") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                iconSourceResult.text = window.windowBase.iconPart.icon
+            }
+        }
+
+        iconSourceResult = textfield {
+            promptText = "icon source result"
+        }
+
+        button("set icon source") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                window.windowBase.iconPart.icon = iconSource.text
+            }
+        }
+
+        iconSource = textfield {
+            promptText = "type here new icon source"
+        }
+
+        togglebutton("switch icon visibility") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                window.windowBase.iconPart.iconIsVisible = this.isSelected
+            }
+        }
+
+        button("get icon is svg") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                iconIsSvgResult.text = window.windowBase.iconPart.iconIsSvg.toString()
+            }
+        }
+
+        iconIsSvgResult = textfield {
+            promptText = "icon is svg result"
+        }
+
+        button("get svg icon zoom") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                svgIconZoomResult.text = window.windowBase.iconPart.svgIconZoom.toString()
+            }
+        }
+
+        svgIconZoomResult = textfield {
+            promptText = "svg icon zoom result"
+        }
+
+        button("set svg icon zoom") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                window.windowBase.iconPart.svgIconZoom = svgIconZoom.text.toDouble()
+            }
+        }
+
+        svgIconZoom = textfield {
+            promptText = "type here new svg icon zoom"
+        }
+
+        button("remove icon") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                window.windowBase.iconPart.removeIcon()
+            }
+        }
+
+        label("If you remove icon, then you need set new icon, for new icon.") {
+            isWrapText = true
+            VBox.setMargin(this, Insets(5.0, 0.0, 0.0, 5.0))
+        }
+    }
+}
---
+++ jfx-window.kdemo/src/main/kotlin/com/mairwunnx/kdemo/TitleView.kt
@@ -0,0 +1,164 @@
+package com.mairwunnx.kdemo
+
+import javafx.geometry.Insets
+import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+import tornadofx.*
+
+class TitleView : View() {
+    private lateinit var titleTextResult: TextField
+    private lateinit var titleText: TextField
+    private lateinit var titleAlignmentResult: TextField
+    private lateinit var titleAlignment: TextField
+    private lateinit var titleTextFontResult: TextArea
+    private lateinit var titleTextFont: TextField
+
+    override val root: VBox = vbox {
+        minHeight = 10.0
+        prefWidth = 161.0
+        hgrow = Priority.ALWAYS
+
+        style {
+            backgroundColor = MultiValue(arrayOf(c("#fff")))
+        }
+
+        label("Title") {
+            font = Font.font("Segoe UI Semilight", 29.0)
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 20.0))
+        }
+
+        button("get title text") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                titleTextResult.text = Application.windowInstance.windowBase.titlePart.titleText
+            }
+        }
+
+        titleTextResult = textfield {
+            promptText = "title text result"
+        }
+
+        button("set title text") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                Application.windowInstance.windowBase.titlePart.titleText = titleText.text
+            }
+        }
+
+        titleText = textfield {
+            promptText = "type here new title text"
+        }
+
+        togglebutton("switch title visibility") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                Application.windowInstance.windowBase.titlePart.titleIsVisible = this.isSelected
+            }
+        }
+
+        button("get title alignment") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                titleAlignmentResult.text = Application.windowInstance.windowBase.titlePart.titleAlignment.name
+            }
+        }
+
+        titleAlignmentResult = textfield {
+            promptText = "title alignment result"
+        }
+
+        button("set title alignment") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                Application.windowInstance.windowBase.titlePart.titleAlignment = enumValueOf(titleAlignment.text)
+            }
+        }
+
+        titleAlignment = textfield {
+            promptText = "type here title alignment"
+        }
+
+        button("get title text font") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                titleTextFontResult.text = Application.windowInstance.windowBase.titlePart.titleTextFont.toString()
+            }
+        }
+
+        titleTextFontResult = textarea {
+            promptText = "get title text font result"
+        }
+
+        button("set title text font") {
+            isFocusTraversable = false
+            isMnemonicParsing = false
+            maxHeight = Double.MAX_VALUE
+            maxWidth = Double.MAX_VALUE
+            prefHeight = 25.0
+            prefWidth = 170.0
+            VBox.setMargin(this, Insets(10.0, 0.0, 0.0, 0.0))
+
+            setOnAction {
+                Application.windowInstance.windowBase.titlePart.titleTextFont =
+                    Font.font(
+                        titleTextFont.text.split(';')[0],
+                        titleTextFont.text.split(';')[1].toDouble()
+                    )
+            }
+        }
+
+        titleTextFont = textfield {
+            promptText = "type here title text font"
+        }
+
+        label("Type font as Segoe UI;16 - When Segoe UI is font, 16 is font size.") {
+            isWrapText = true
+            VBox.setMargin(this, Insets(5.0, 0.0, 0.0, 5.0))
+        }
+    }
+}
