60a256b652df66b21b5dc882b40ac70c108599cf
Pavel Erokhin <MairwunNx@gmail.com>
2019-05-11 20:12:26 +0300

    setMinimizableInTaskBar wrapped in try catch block.

--- jfx-window/src/main/kotlin/jfxwindow/listeners/WindowMinimizeListener.kt
+++ jfx-window/src/main/kotlin/jfxwindow/listeners/WindowMinimizeListener.kt
@@ -6,6 +6,8 @@ import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinUser.GWL_STYLE
 import javafx.stage.Screen
 import jfxwindow.base.WindowBase
+import java.util.logging.Level
+import java.util.logging.Logger
 
 @Suppress("RedundantUnitReturnType")
 internal class WindowMinimizeListener(private val windowBase: WindowBase) {
@@ -24,12 +26,19 @@ internal class WindowMinimizeListener(private val windowBase: WindowBase) {
     }
 
     private fun setMinimizableInTaskBar() {
-        val lhwnd = com.sun.glass.ui.Window.getWindows()[0].nativeWindow
-        val lpVoid = Pointer(lhwnd)
-        val hwnd = WinDef.HWND(lpVoid)
-        val user32 = User32.INSTANCE
-        val oldStyle = user32.GetWindowLong(hwnd, GWL_STYLE)
-        val newStyle = oldStyle or 0x00020000
-        user32.SetWindowLong(hwnd, GWL_STYLE, newStyle)
+        try {
+            val lhwnd = com.sun.glass.ui.Window.getWindows()[0].nativeWindow
+            val lpVoid = Pointer(lhwnd)
+            val hwnd = WinDef.HWND(lpVoid)
+            val user32 = User32.INSTANCE
+            val oldStyle = user32.GetWindowLong(hwnd, GWL_STYLE)
+            val newStyle = oldStyle or 0x00020000
+            user32.SetWindowLong(hwnd, GWL_STYLE, newStyle)
+        } catch (ex: Exception) {
+            Logger.getLogger(javaClass.name).log(
+                Level.WARNING,
+                "It was not possible to add the ability to minimize the window through the taskbar. Perhaps not used by Windows, or the old version of Windows."
+            )
+        }
     }
 }
